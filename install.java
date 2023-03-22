082f1d1ba2dc286f851c60110a635849e1903166
Dami Adekanye <Adekanyedamilola@gmail.com>
2023-03-22 01:58:56 +0000

    Java install script is ready for deployment

---
+++ install.java
@@ -0,0 +1,8 @@
+# install Java JDK 1.8+ as a pre-requisit for maven to run.
+
+sudo hostname maven
+cd /opt
+sudo yum install wget nano tree unzip git-all -y
+sudo yum install java-11-openjdk-devel java-1.8.0-openjdk-devel -y
+java -version
+git --version
